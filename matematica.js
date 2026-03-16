f77a211cd9e70d5ca0e723960f4a14004f4ea1d9
FELIPE FELDMAN <48860060@ort>
2026-03-16 08:58:01 -0300

    avance feldman

---
+++ matematica.js
@@ -0,0 +1,13 @@
+let sumar = (a, b) => a + b;
+
+let restar = (a, b) => a - b;
+
+let multiplicar = (a, b) => a * b;
+
+let dividir = (a, b) => a / b;
+
+const PI = 3.1416;
+
+
+
+export { sumar, restar, multiplicar, dividir, PI };
