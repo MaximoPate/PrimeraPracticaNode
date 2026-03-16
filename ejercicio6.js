78be8738ddf533fce8b38a819acfb8429d974736
FELIPE FELDMAN <48860060@ort>
2026-03-16 10:19:02 -0300

    finishim2.0

--- ejercicio6.js
+++ ejercicio6.js
@@ -10,12 +10,12 @@ function parsearUrl(url) {
     };
   } catch (error) {
     console.error('Error al parsear la URL:', error.message);
-    return null; // or throw error, but since we need to show in console, maybe return null
+    return null; 
   }
 }
 
 
 console.log(parsearUrl('http://www.ort.edu.ar:8080/alumnos/index.htm?curso=2022&mes=mayo'));
-console.log(parsearUrl('invalid-url')); // to test the exception
+console.log(parsearUrl('invalid-url'));
 }
  ejercicio6();
