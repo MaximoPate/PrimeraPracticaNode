11c69a24d2e04f3f6afbc0b02e6c7abbbadb30b4
MAXIMO URIEL PATEJIM <49090825@ort>
2026-03-16 09:00:23 -0300

    avance maxi

---
+++ ejercicio3.js
@@ -0,0 +1,16 @@
+function ejercicio3() {
+
+const alumno1 = {
+    nombre: "Juan",
+    dni: "49090825"
+}
+const alumno2 = {
+    nombre: "Maria",
+    dni: "49025135"
+}
+
+console.log(alumno1);
+console.log(alumno2);
+}
+
+export default ejercicio3;
---
+++ index.js
@@ -0,0 +1,6 @@
+//import ejercicio1 from './ejercicio1.js';
+//ejercicio1("Hola", "Mundo");
+
+//import ejercicio3 from './ejercicio3.js';
+//ejercicio3();
+
