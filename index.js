cb007557dd067ec77fabef6c67b06bb5f2bfe20a
MAXIMO URIEL PATEJIM <49090825@ort>
2026-03-16 09:49:43 -0300

    fin maxi

--- index.js
+++ index.js
@@ -4,3 +4,8 @@
 //import ejercicio3 from './ejercicio3.js';
 //ejercicio3();
 
+//import ejercicio5 from './ejercicio5.js';
+//ejercicio5();
+
+//import countryToCurrency from "country-to-currency"; 
+//console.log(countryToCurrency["AR"]);
